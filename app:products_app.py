0c866bca6edbb68ccd6a8f5bc9f2d99f0c21686e
marinabart <marinabartzokis@gmail.com>
2017-07-19 19:03:38 -0400

    Added files to repo

---
+++ app:products_app.py
@@ -0,0 +1,70 @@
+menu = """"
+    Hi
+
+    Welcome to the products app.
+
+    There are 100 products.
+
+    Available operations: 'List', 'Show', 'Create', 'Update', 'Destroy'
+
+    Please choose an operation:
+
+"""
+
+chosen_operation = input(menu)
+
+if chosen_operation.title() == "List":
+    print("LISTING PRODUCTS")
+elif chosen_operation.title() == "Show":
+    print("SHOWING A PRODUCT")
+elif chosen_operation.title() == "Create":
+            print("CREATING A PRODUCT")
+elif chosen_operation.title() == "Update":
+        print("UPDATING A PRODUCT")
+elif chosen_operation.title() == "Destroy":
+        print("DESTROYING A PRODUCT")
+else:
+    print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
+
+
+
+
+
+
+print(                          )
+print("-----------------------------------------")
+print("PRODUCTS APPLICATION")
+print("-----------------------------------------")
+print("Welcome marianbart!")
+print(                          )
+
+print("There are 20 products in the database. Please select an operation:")
+print(                          )
+
+print("operation | description")
+print("--------- |  -------------------")
+print("'List'    | Display a list of product identifiers and names.")
+print("'Show'    | Show information about a product.")
+print("'Create'  | Add a new product.")
+print("'Update'  | Edit an existing product.")
+print("'Destroy' | Delete an existing product.")
+
+
+
+if List:
+    print("THERE ARE 20 PRODUCTS:")
+
+if Show:
+    print("OK. Please specify the product's identifier:")
+
+if Create:
+    print("OK. Please specify the product's identifier:")
+
+
+if Update:
+    print()
+
+
+
+if Destroy:
+    print("OK. Please specify the product's iedntifier:")
