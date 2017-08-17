2f422320ef70368f8def19b3ae7c05d00020122b
Marina <marinabartzokis@gmail.com>
2017-08-17 18:46:55 -0400

    Updated with actual project
    I saw that my last update on my laptop was on August 2nd, but my last commit here was from 29 days ago (today is Aug 17). You were evaluating half-finished code. See here for final.

--- app:products_app.py
+++ app:products_app.py
@@ -1,70 +1,118 @@
-menu = """"
-    Hi
+#from IPython import embed
+import csv
 
-    Welcome to the products app.
+products = []
 
-    There are 100 products.
+products_csv = "/Users/marinabartzokis/Desktop/CRUD_App/data/products.csv"
+headers = ["id", "name", "aisle", "department", "price"] # for "Further Exploration" use: ["product_id", "product_name", "aisle_id", "aisle_name", "department_id", "department_name", "price"]
+user_input_headers = [header for header in headers if header != "id"] # don't prompt the user for the product_id
 
-    Available operations: 'List', 'Show', 'Create', 'Update', 'Destroy'
+def get_product_id(product): return int(product["id"])
 
-    Please choose an operation:
+def auto_incremented_id(products):
+    product_ids = map(get_product_id, products)
+#    print(product_ids)
+    return max(product_ids) + 1
 
-"""
 
-chosen_operation = input(menu)
+#
+# READ PRODUCTS FROM FILE
+#
 
-if chosen_operation.title() == "List":
-    print("LISTING PRODUCTS")
-elif chosen_operation.title() == "Show":
-    print("SHOWING A PRODUCT")
-elif chosen_operation.title() == "Create":
-            print("CREATING A PRODUCT")
-elif chosen_operation.title() == "Update":
-        print("UPDATING A PRODUCT")
-elif chosen_operation.title() == "Destroy":
-        print("DESTROYING A PRODUCT")
+with open(products_csv, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for ordered_dict in reader:
+        products.append(dict(ordered_dict))
+
+#
+# HANDLE USER INPUT
+#
+
+
+
+def list_products():
+    print("LISTING PRODUCTS HERE")
+    for product in products:
+        print(" + Product #" + str(product["id"]) + ": " + product["name"])
+
+def show_product():
+    product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
+    product = [p for p in products if p["id"] == product_id][0]
+    if product:
+        print("READING PRODUCT HERE", product)
+    else:
+        print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product)
+
+def create_product():
+    print("OK. PLEASE PROVIDE THE PRODUCT'S INFORMATION...")
+    print(products)
+    product = {"id": auto_incremented_id(products)}
+    for header in user_input_headers:
+        product[header] = input("The '{0}' is: ".format(header))
+    products.append(product)
+    print("CREATING PRODUCT HERE", product)
+
+def update_product():
+    product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
+    product = [p for p in products if p["id"] == product_id][0]
+    if product:
+        print("OK. PLEASE PROVIDE THE PRODUCT'S INFORMATION...")
+        for header in user_input_headers:
+            product[header] = input("Change '{0}' from '{1}' to: ".format(header, product[header]))
+        print("UPDATING PRODUCT HERE", product)
+    else:
+        print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product_id)
+
+def destroy_product():
+    product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
+    product = [p for p in products if p["id"] == product_id][0]
+    if product:
+        print("DESTROYING PRODUCT HERE", product)
+        del products[products.index(product)]
+    else:
+        print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product_id)
+
+
+
+menu = """
+-----------------------------------
+PRODUCTS APPLICATION
+-----------------------------------
+Welcome {0}!
+There are {1} products in the database.
+    operation | description
+    --------- | ------------------
+    'List'    | Display a list of product identifiers and names.
+    'Show'    | Show information about a product.
+    'Create'  | Add a new product.
+    'Update'  | Edit an existing product.
+    'Destroy' | Delete an existing product.
+Please select an operation: """.format("@marinabart", len(products)) # end of multi- line string. also using string interpolation
+
+crud_operation = input(menu)
+
+if crud_operation.title() == "List":
+    list_products()
+elif crud_operation.title() == "Show":
+    show_product()
+elif crud_operation.title() == "Create":
+    create_product()
+elif crud_operation.title() == "Update":
+    update_product()
+elif crud_operation.title() == "Destroy":
+    destroy_product()
 else:
-    print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
+    print("OOPS SORRY. PLEASE TRY AGAIN.")
 
+#
+# WRITE PRODUCTS TO FILE
+#
 
+otherproducts_csv = "/Users/marinabartzokis/Desktop/CRUD_App/data/otherproducts.csv"
 
+with open(otherproducts_csv, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=headers)
+    writer.writeheader()
 
-
-
-print(                          )
-print("-----------------------------------------")
-print("PRODUCTS APPLICATION")
-print("-----------------------------------------")
-print("Welcome marianbart!")
-print(                          )
-
-print("There are 20 products in the database. Please select an operation:")
-print(                          )
-
-print("operation | description")
-print("--------- |  -------------------")
-print("'List'    | Display a list of product identifiers and names.")
-print("'Show'    | Show information about a product.")
-print("'Create'  | Add a new product.")
-print("'Update'  | Edit an existing product.")
-print("'Destroy' | Delete an existing product.")
-
-
-
-if List:
-    print("THERE ARE 20 PRODUCTS:")
-
-if Show:
-    print("OK. Please specify the product's identifier:")
-
-if Create:
-    print("OK. Please specify the product's identifier:")
-
-
-if Update:
-    print()
-
-
-
-if Destroy:
-    print("OK. Please specify the product's iedntifier:")
+    for product in products:
+        writer.writerow(product)
